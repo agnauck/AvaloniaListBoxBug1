cadb7aa255bd1906b6a488eff5f8343302b12f6f
Alexander Gnauck <gnauck@gmail.com>
2023-03-21 11:20:48 +0100

    simplify bug sample

--- AvaloniaListBoxBug1/Models/Contacts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AvaloniaListBoxBug1.Models
-{
-    public class Contacts : Collection<Contact, string>
-    {
-        public Contacts() : base(c => c.Jid)
-        {
-        }
-    }
-}
